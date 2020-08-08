74ae90a0b847ec10d9e1ff4086e7d1003db9b29d
Caroline Binley <caroline.binley@researchsquare.com>
2020-08-08 12:05:57 -0400

    Lint module

--- src/news_retrieval_service.py
+++ src/news_retrieval_service.py
@@ -1,4 +1,10 @@
-# TODO(DLB): pylint
+"""
+Service to retrieve news articles from GDELT news tracker.
+Usage:
+    # write how to use service
+"""
+
+# TOD(DLB): pylint
 # TODO(DLB): delete unused variables
 
 import json
@@ -84,33 +90,37 @@ GDELT_COLUMN_NAMES = [
     'SOURCEURL',
 ]
 
-# TODO(DLB): decide how to handle GDELT files -- always go to most_recent_file_list? read master_file_list and grab N new entries? etc.
+# TODO(DLB): decide how to handle GDELT files -- always go to most_recent_file_list?
+# read master_file_list and grab N new entries? etc.
 
 
 # GDELT_MASTER_FILE_LIST_URL = 'http://data.gdeltproject.org/gdeltv2/masterfilelist.txt'
 
 GDELT_MOST_RECENT_FILE_LIST_URL = 'http://data.gdeltproject.org/gdeltv2/lastupdate.txt'
 
-GDELT_MOST_RECENT_FILE_LIST  = requests.get(GDELT_MOST_RECENT_FILE_LIST_URL)
+GDELT_MOST_RECENT_FILE_LIST = requests.get(GDELT_MOST_RECENT_FILE_LIST_URL)
 
 NEWS_ARTICLES = []
 
 # TODO(DLB): determine which GDELT_COLUMNS are important
 def create_gdelt_dataframe(url):
+    """Create dataframe from URL containing zipped CSV of GDELT data"""
     df_gdelt = pd.read_csv(url, names=GDELT_COLUMN_NAMES, delimiter='\t')
     return df_gdelt[['DATEADDED', 'SOURCEURL']]
 
 def clean_date(date):
+    """Format date for file name"""
     date = str(date)
     return date
 
 def get_publisher(url):
+    """Format URL for file name"""
     starting_substrings = [
-              'https://www.',
-              'http://www.',
-              'http://www3.',
-              'https://',
-              'http://',
+        'https://www.',
+        'http://www.',
+        'http://www3.',
+        'https://',
+        'http://',
     ]
     for substring in starting_substrings:
         start = url.find(substring)
@@ -125,38 +135,50 @@ def get_publisher(url):
     return url
 
 def clean_title(title):
+    """Format title for file name"""
     title = title.lower()
     title = re.sub(r'\W', '-', title)
     title = re.sub(r'-+', '-', title)
     return title
 
 def create_article_file_name(title, url, date):
+    """Create article file name"""
     file_name = '_'.join((clean_date(date), get_publisher(url), clean_title(title)))
-    file_name = f'{file_name}.txt'
+    file_name = f'{file_name}.json'
     return file_name
 
 def write_output_file(file_name, file_directory, file_content):
+    """Write JSON file"""
     with open(os.path.join(file_directory, file_name), 'w') as file:
         json.dump(file_content, file)
 
 # TODO(DLB): port this to function
-for line in GDELT_MOST_RECENT_FILE_LIST.text.splitlines():
-    *_, NEWEST_GDELT_STORY_LIST_URL = line.split()
-    df = pd.read_csv(NEWEST_GDELT_STORY_LIST_URL, names=GDELT_COLUMN_NAMES, delimiter='\t')
-    for i, (url, date) in enumerate(set(zip(df['SOURCEURL'], df['DATEADDED']))):
-        if any(url.startswith(banned_url) for banned_url in BANNED_URLS):
-            continue
-        print(f"Gathering content from {url}")
-        try:
-            article = Article(url)
-            article.download()
-            article.parse()
-            file_name = create_article_file_name(article.title, url, date)
-            write_output_file(file_name, 'data/news_articles/', {'url': url, 'date': date, 'title': article.title, 'text': article.text})
-            ### handle file writing
-        except Exception as e:
-            print(repr(e))
-        if i > 5:
-            break
-    break
-
+def __main__():
+    """Function string to rewrite after refactor"""
+    for line in GDELT_MOST_RECENT_FILE_LIST.text.splitlines():
+        *_, newest_gdelt_story_list_url = line.split()
+        df_gdelt = pd.read_csv(
+            newest_gdelt_story_list_url,
+            names=GDELT_COLUMN_NAMES,
+            delimiter='\t'
+        )
+        for i, (url, date) in enumerate(set(zip(df_gdelt['SOURCEURL'], df_gdelt['DATEADDED']))):
+            if any(url.startswith(banned_url) for banned_url in BANNED_URLS):
+                continue
+            print(f"Gathering content from {url}")
+            try:
+                article = Article(url)
+                article.download()
+                article.parse()
+                file_name = create_article_file_name(article.title, url, date)
+                write_output_file(
+                    file_name,
+                    'data/news_articles/',
+                    {'url': url, 'date': date, 'title': article.title, 'text': article.text}
+                )
+                ### handle file writing
+            except Exception as exception:
+                print(repr(exception))
+            if i > 5:
+                break
+        break
