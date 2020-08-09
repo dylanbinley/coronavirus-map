30449cf5772eb1efab6de0b8ccb82d5f0d0f75df
Caroline Binley <caroline.binley@researchsquare.com>
2020-08-09 09:55:35 -0400

    Rename application service

--- src/application/data_generation_service.py
+++ src/application/data_generation_service.py
@@ -20,21 +20,19 @@ def write_output_file(file_path, file_content):
     with open(file_path, 'w') as file:
         json.dump(file_content, file)
 
-class NewsRetrievalService:
-    
+class DataGenerationService:
+
     def __init__(self):
         pass
-    
-    def retrieve_latest_news(self, output_directory, sample_size, n_hours, n_days):
+
+    def generate_data(self, output_directory, sample_size, n_hours, n_days):
         retriever = news_retrieval_service.NewsRetrievalService(sample_size=sample_size)
         if not n_hours and not n_days:
-            latest_news = retriever.scrape_latest_gdelt_dataset()
+            news = retriever.scrape_latest_gdelt_dataset()
         elif n_hours:
-            retriever = news_retrieval_service.NewsRetrievalService()
-            latest_news = retriever.scrape_latest_gdelt_datasets(4*n_hours)
+            news = retriever.scrape_latest_gdelt_datasets(4*n_hours)
         else:
-            retriever = news_retrieval_service.NewsRetrievalService()
-            latest_news = retriever.scrape_latest_gdelt_datasets(4*24*n_days)
-        for article in latest_news:
+            news = retriever.scrape_latest_gdelt_datasets(4*24*n_days)
+        for article in news:
             file_path = os.path.join(output_directory, f"{article['GlobalEventID']}.json")
             write_output_file(file_path, article)
--- src/cli.py
+++ src/cli.py
@@ -1,12 +1,12 @@
 import click
 
-import src.application.news_retrieval_service as news_retrieval_service
+import src.application.data_generation_service as data_generation_service
 
 @click.command()
 @click.option('--output_directory', required=True, type=click.STRING)
 @click.option('--sample_size', required=True, type=click.FLOAT)
 @click.option('--n_hours', required=False, type=click.INT)
 @click.option('--n_days', required=False, type=click.INT)
-def retrieve_latest_news(output_directory, sample_size, n_hours=None, n_days=None):
-    retriever = news_retrieval_service.NewsRetrievalService()
-    retriever.retrieve_latest_news(output_directory, sample_size, n_hours, n_days)
+def generate_data(output_directory, sample_size, n_hours=None, n_days=None):
+    generator = data_generation_service.DataGenerationService()
+    generator.generate_data(output_directory, sample_size, n_hours, n_days)
