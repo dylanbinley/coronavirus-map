2fac217acc891abc689fb7b1c7dbd60154e8d581
Your Name <you@example.com>
2020-08-16 14:57:40 +0000

    fixed again

--- coronavirus_map/filter.py
+++ coronavirus_map/filter.py
@@ -5,12 +5,12 @@ def find_coronavirus_stories(directory):
     files_in_directory = os.listdir(directory)
     coronavirus_stories = []
     for file_path in files_in_directory:
-        file_path= os.path.join(directory,filepath)
+        file_path = os.path.join(directory, file_path)
         with open(file_path, 'r') as file:
             article = json.load(file)
-        article_title = article.get('ARTICLE', {}).get('TITLE', '')
+        title = article.get('ARTICLE', {}).get('TITLE', '')
         if story_is_covid(title):
-            coronavirus_stories.append(filepath)
+            coronavirus_stories.append(file_path)
     return coronavirus_stories
 
 def story_is_covid(title):
