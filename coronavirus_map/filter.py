10da064d08ed98e889287bca15b0a8a99f5ad79e
Your Name <you@example.com>
2020-08-16 14:55:05 +0000

    fixed variable name:

--- coronavirus_map/filter.py
+++ coronavirus_map/filter.py
@@ -4,8 +4,8 @@ import json
 def find_coronavirus_stories(directory):
     files_in_directory = os.listdir(directory)
     coronavirus_stories = []
-    for filepath in files_in_directory:
-        filepath = os.path.join(directory,filepath)
+    for file_path in files_in_directory:
+        file_path= os.path.join(directory,filepath)
         with open(file_path, 'r') as file:
             article = json.load(file)
         article_title = article.get('ARTICLE', {}).get('TITLE', '')
