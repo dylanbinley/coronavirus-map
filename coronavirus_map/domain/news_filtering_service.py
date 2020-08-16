b0c9aa7291c671dd863429c98cf72a8921c0dce9
Caroline Binley <caroline.binley@researchsquare.com>
2020-08-16 11:13:43 -0400

    Remove file directory dependency

--- coronavirus_map/domain/news_filtering_service.py
+++ coronavirus_map/domain/news_filtering_service.py
@@ -6,17 +6,13 @@ class NewsFilteringService:
     def __init__(self):
         pass
 
-    def find_coronavirus_stories(self, directory):
-        files_in_directory = os.listdir(directory)
-        coronavirus_stories = []
-        for file_path in files_in_directory:
-            file_path = os.path.join(directory, file_path)
-            with open(file_path, 'r') as file:
-                article = json.load(file)
+    def find_coronavirus_stories(self, data_dicts):
+        coronavirus_data_dicts = []
+        for article in data_dicts:
             title = article.get('ARTICLE', {}).get('TITLE', '')
             if self._story_is_covid(title):
-                coronavirus_stories.append(file_path)
-        return coronavirus_stories
+                coronavirus_data_dicts.append(article)
+        return coronavirus_data_dicts
 
     def _story_is_covid(self, title):
         title = title.lower()
