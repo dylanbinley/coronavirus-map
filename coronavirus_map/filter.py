08bd1428777214ec6f9590ed03d79b587477aeac
Caroline Binley <caroline.binley@researchsquare.com>
2020-08-16 09:00:42 -0400

    Remove semicolons

--- coronavirus_map/filter.py
+++ coronavirus_map/filter.py
@@ -1,21 +1,22 @@
-import os;
-import json;
+import os
+import json
+
 def main(directory):
-    directory_file = os.listdir(directory);
-    coronavirus_stories = [];
+    directory_file = os.listdir(directory)
+    coronavirus_stories = []
     for filepath in directory_file:
-        filepath = os.path.join(directory,filepath);
+        filepath = os.path.join(directory,filepath)
         file = open(filepath, 'r')
         try:
-            title = json.load(file)['ARTICLE']['TITLE'];
+            title = json.load(file)['ARTICLE']['TITLE']
         except:
-            continue;
+            continue
         if (story_is_covid(title)):
-            coronavirus_stories.append(filepath);
-        file.close();
-    return coronavirus_stories;
+            coronavirus_stories.append(filepath)
+        file.close()
+    return coronavirus_stories
 
 def story_is_covid(title):
-    title = title.lower();
+    title = title.lower()
     return ('coronavirus' in title or
            'covid' in title)
